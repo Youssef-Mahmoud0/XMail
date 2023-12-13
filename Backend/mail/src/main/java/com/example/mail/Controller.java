e96d37860232ed9d0e293df9a44c488376d2a784
rowanxgamal <rowangamalelkhouly@gmail.com>
2023-12-14 01:53:53 +0200

    back update

---
+++ Backend/mail/src/main/java/com/example/mail/Controller.java
@@ -0,0 +1,18 @@
+package com.example.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+public class Controller {
+//    @Autowired
+//    private ArrayList<User>users;
+    @PostMapping("/signup")
+    public boolean signed(@RequestBody User user){
+
+    }
+}
